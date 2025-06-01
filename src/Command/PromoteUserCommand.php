ccc40d46e72808212da825093bbc219dfe63458e
NourMahmoud151515 <nour.mahmoud@insat.ucar.tn>
2025-06-01 10:33:08 +0100

    Add src/Command folder and its content

---
+++ src/Command/PromoteUserCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:promote-user',
+    description: 'Promotes a user to ROLE_ADMIN',
+)]
+class PromoteUserCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the user to promote')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            $io->error(sprintf('User with email "%s" not found.', $email));
+            return Command::FAILURE;
+        }
+
+        $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
+        $this->entityManager->flush();
+
+        $io->success(sprintf('User "%s" has been promoted to ROLE_ADMIN.', $email));
+
+        return Command::SUCCESS;
+    }
+} 
